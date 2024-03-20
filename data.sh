4164c90adbe73c169feea3c35b951fe788c9fb27
KARTHIK0418 <147519514+KARTHIK0418@users.noreply.github.com>
2024-03-20 16:28:30 +0530

    Update data.sh

--- data.sh
+++ data.sh
@@ -3,7 +3,7 @@ sudo yum install docker -y
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo systemctl status docker
-sudo chnod 666 /var/run/docker.sock
+sudo chnod 777 /var/run/docker.sock
 sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose version
