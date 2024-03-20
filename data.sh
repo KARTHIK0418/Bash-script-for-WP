3ea1fa32ba5be5cd2a620a10788ebb6c8e9c3417
KARTHIK0418 <147519514+KARTHIK0418@users.noreply.github.com>
2024-03-20 15:49:55 +0530

    Create data.sh

---
+++ data.sh
@@ -0,0 +1,12 @@
+sudo yum update -y
+sudo yum instll docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo systemctl status docker
+sudo chnod 666 /var/run/docker.sock
+sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose version
+cd
+sudo yum install git -y
+git clone 
